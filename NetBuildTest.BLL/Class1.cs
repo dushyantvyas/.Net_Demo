bca8863b5f1853b12ef17bb4b4e2859a78dc599e
Dushyant Vyas <dushyant.vyas@accenture.com>
2016-07-27 02:09:27 -0500

    Added Code

---
+++ NetBuildTest.BLL/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBuildTest.BLL
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public string HelloWorld(string input)
+        {
+            return input;
+        }
+
+        public string Jsonify(object input)
+        {
+            return JsonConvert.SerializeObject(input);
+        }
+
+    }
+}
---
+++ NetBuildTest.BLLTests/Class1Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetBuildTest.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBuildTest.BLL.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void HelloWorldTest()
+        {
+            const string input = "test";
+            string output = null;
+            BLL.Class1 instance = null;
+
+            instance = new Class1();
+            output = instance.HelloWorld(input);
+
+            Assert.AreEqual(input, output);
+        }
+
+        [TestMethod()]
+        public void JsonifyTest()
+        {
+            const string input = "test";
+            const string validate = @"[""test""]";
+            string output = null;
+            BLL.Class1 instance = null;
+            List<string> lst = null;
+
+            lst = new List<string>();
+            lst.Add(input);
+            instance = new Class1();
+            output = instance.Jsonify(lst);
+
+            Assert.AreEqual(validate, output);
+        }
+    }
+}
